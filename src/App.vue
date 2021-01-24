50ed64162a63732516adda7d9fa78c64d2e36f7a
Charlie Stigler <charlie@charliestigler.com>
2021-01-23 16:06:12 -0800

    Downgrade to Vue 2 and plain JS cause it's easier

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <ShrubControlPanel msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import ShrubControlPanel from './components/ShrubControlPanel.vue';
 
-export default defineComponent({
+export default {
   name: 'App',
   components: {
-    HelloWorld,
-  },
-});
+    ShrubControlPanel,
+  }
+};
 </script>
 
 <style>
