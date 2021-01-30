d049a8a911e99f161342bb162e52462bb7cf542f
Charlie Stigler <charlie@charliestigler.com>
2021-01-29 18:43:19 -0800

    Allow use of a separate websocket URL vs client URL

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import VueSocketIOExt from 'vue-socket.io-extended';
 import { io } from 'socket.io-client';
 import App from './App.vue';
 import router from './router'
+import config from '../config';
+
+let websocketAPIURL = (process.env.NODE_ENV === 'production') ? config.prodWebSocketAPI : null;
 
 Vue.config.productionTip = false;
 
-Vue.use(VueSocketIOExt, io());
+Vue.use(VueSocketIOExt, io(websocketAPIURL));
 
 new Vue({
   router,
