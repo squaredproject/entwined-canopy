dceffe9745b968aefcd9a08b6cc3fa316265cdc1
Charlie Stigler <charlie@charliestigler.com>
2021-01-29 22:51:49 -0800

    Remove Express entirely - whoa

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET shrub control pages */
-router.get('/shrubs/:shrubId', function(req, res, next) {
-  res.sendFile('index.html', { root: 'dist' });
-});
-
-module.exports = router;
