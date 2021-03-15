134e7bafb65d2113484f903c8c7016645bdb49e0
Charlie Stigler <charlie@charliestigler.com>
2021-03-14 18:04:07 -0700

    Only show fire triggerable since it's the only one that works

--- client/src/components/ShrubControlPanel.vue
+++ client/src/components/ShrubControlPanel.vue
@@ -20,11 +20,8 @@
       </div>
     </div>
     <div class="one-shot-triggers">
-      <h3 class="triggerables-title">Tap to Run Special Patterns</h3>
-      <button type="button" class="btn btn-outline-primary" ontouchstart="" v-on:click="runOneShotTriggerable('lightning')">⚡️</button>
-      <button type="button" class="btn btn-outline-primary" ontouchstart="" v-on:click="runOneShotTriggerable('rain')">🌧</button>
-      <button type="button" class="btn btn-outline-primary" ontouchstart="" v-on:click="runOneShotTriggerable('bass-slam')">🚨</button>
-      <button type="button" class="btn btn-outline-primary" ontouchstart="" v-on:click="runOneShotTriggerable('color-burst')">🌈</button>
+      <h3 class="triggerables-title">Tap to Run Special Pattern</h3>
+      <button type="button" class="btn btn-outline-primary" ontouchstart="" v-on:click="runOneShotTriggerable('fire')">🔥</button>
     </div>
     <div>
       <span>{{sessionTimeRemainingString}} remaining in session</span>
