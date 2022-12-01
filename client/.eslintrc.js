7ecfa8cf5164770007f2234c4b1f263af6e3ebbf
Charlie Stigler <charlie@charliestigler.com>
2022-12-01 01:25:05 -0800

    Add client eslintrc

---
+++ client/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',
+  }
+}
