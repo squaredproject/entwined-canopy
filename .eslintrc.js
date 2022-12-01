5d87ac10172a4bea9edd058f39febbae02648cf5
Charlie Stigler <charlie@charliestigler.com>
2022-12-01 01:41:45 -0800

    Play with build settings to stop linter breaking on Netlify deploys

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,18 @@ module.exports = {
     node: true,
   },
 
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+  // parserOptions: {
+  //   parser: 'babel-eslint',
+  // },
 
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'vue/multi-word-component-names': 'off',
   },
 
   extends: [
-    'plugin:vue/essential',
-    'eslint:recommended',
+    // 'plugin:vue/essential',
+    // 'eslint:recommended',
   ],
 };
--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,11 +1,20 @@
 module.exports = {
+  env: {
+    node: true,
+  },
+
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
   },
 
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off',
-  }
+  },
+
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+  ],
 }
